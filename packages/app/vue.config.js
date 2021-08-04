3f8eee8a50b9395a323e129507cc3218075fa6a1
zyhou <richard.maxime9@gmail.com>
2021-08-04 12:47:06 +0200

    fix asset path

--- packages/app/vue.config.js
+++ packages/app/vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    publicPath: process.env.NODE_ENV === 'production' ? '/15-puzzle-vue/' : '/',
+    publicPath: '/',
     pages: {
         index: {
             entry: 'src/main.js',
